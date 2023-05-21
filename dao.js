5c9536fbcadbbcfeafe00bf1608a88334d66ecdd
nathanruf <81049820+nathanruf@users.noreply.github.com>
2023-05-21 19:17:00 -0300

    dao commit

---
+++ dao.js
@@ -0,0 +1,19 @@
+const bd = require("./models/Database")
+
+class DAO{
+    static create(){
+
+    }
+    
+    static read(){
+
+    }
+
+    static update(){
+
+    }
+
+    static delete(){
+
+    }
+}
