a6b6b4765b719c2772a567d82d54c32e672f80a0
Vinicius Marrafon <96776865+Vinicius-M-Marrafon@users.noreply.github.com>
2023-06-18 11:22:49 -0300

    Create CompradorDAO.mjs

---
+++ controller/DAO/CompradorDAO.mjs
@@ -0,0 +1,29 @@
+const db = require('../models/Database')
+
+module.exports = class CompradorDAO
+{
+    static async getById(id){
+        let results = await db.query("SELECT * FROM clientes WHERE id=?", [id]);
+        return results;
+    }
+
+    static async getByEmail(email){
+        let results = await db.query("SELECT * FROM clientes WHERE email=?", [email]);
+        return results;
+    }
+
+    static async insert(comprador){
+        let results = await db.query("INSERT INTO clientes (nome, email, senha) VALUES (?, ?, ?)",
+        [comprador.getNome(), comprador.getEmail(), bcrypt.hashSync(comprador.getSenha(), 10)]);
+        return results.insertId;
+    }
+
+    static async update(comprador){
+        let results = await db.query("UPDATE clientes SET nome=?, email=?, senha=? WHERE id=?", 
+        [comprador.getNome(), comprador.getEmail(), bcrypt.hashSync(comprador.getSenha(), 10), comprador.getId()]);
+    }
+
+    static async delete(id){
+        let results = await db.query("DELETE FROM clientes WHERE id=?", [id]);
+    }
+}
