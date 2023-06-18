5f0e61dc1433443774cc3acba21ece33522367aa
Rafael Ferreira <rafik401@hotmail.com>
2023-06-18 17:29:52 -0300

    comprador sqç

--- bd.sql
+++ bd.sql
@@ -34,10 +34,10 @@ CREATE TABLE produtos (
     FOREIGN KEY (id_itens) REFERENCES produtos(id)
  );
  
-INSERT INTO clientes values (Default, "Adm", "adm@gmail.com", "$2a$10$zvv8Bk6.TTrxu2kAhO8H9uiuhwaJ5vIkfe1OMLXmcCxKrpaz1pzv2", "111.111.111-11", "Casa do adm", "11911111111", 1, DEFAULT);
-INSERT INTO clientes values (Default, "Geraldo", "emailfalso1@gmail.com", "$2a$10$MI.S.ZUqNbmDSbY/2vnrbuf7peJ4o2C3w2uPWr04416JfLl0mhEHW", "222.222.222-22", "Casa 1", "22922222222", DEFAULT, DEFAULT);
-INSERT INTO clientes values (Default, "Fagundes", "emailfalso2@gmail.com", "$2a$10$wuafwCyc26u2LLTi6S2ShuTlZQnCsv/6WCB7ZBzc8YAB4h/mTkmT2", "333.333.333-33", "Casa 2", "33933333333", DEFAULT, DEFAULT);
-INSERT INTO clientes values (Default, "Lurdes", "emailfalso3@gmail.com", "$2a$10$khUEVvF.yYpuHLqoOW0Zxe2YpYrFj/nF5CZujuKdjlfi0QXI1vhla", "444.444.444-44", "Casa 3", "44944444444", DEFAULT, DEFAULT);
+INSERT INTO comprador values (Default, "Adm", "adm@gmail.com", "$2a$10$zvv8Bk6.TTrxu2kAhO8H9uiuhwaJ5vIkfe1OMLXmcCxKrpaz1pzv2", "111.111.111-11", "Casa do adm", "11911111111", 1, DEFAULT);
+INSERT INTO comprador values (Default, "Geraldo", "emailfalso1@gmail.com", "$2a$10$MI.S.ZUqNbmDSbY/2vnrbuf7peJ4o2C3w2uPWr04416JfLl0mhEHW", "222.222.222-22", "Casa 1", "22922222222", DEFAULT, DEFAULT);
+INSERT INTO comprador values (Default, "Fagundes", "emailfalso2@gmail.com", "$2a$10$wuafwCyc26u2LLTi6S2ShuTlZQnCsv/6WCB7ZBzc8YAB4h/mTkmT2", "333.333.333-33", "Casa 2", "33933333333", DEFAULT, DEFAULT);
+INSERT INTO comprador values (Default, "Lurdes", "emailfalso3@gmail.com", "$2a$10$khUEVvF.yYpuHLqoOW0Zxe2YpYrFj/nF5CZujuKdjlfi0QXI1vhla", "444.444.444-44", "Casa 3", "44944444444", DEFAULT, DEFAULT);
 INSERT INTO produtos(nome,valor,prazo_validade) values("pão de mel", 12.00, '2022-12-12');
 INSERT INTO produtos(nome,valor,prazo_validade)  values( "bolo de morango", 12.00, '2022-12-12');
 INSERT INTO produtos(nome,valor,prazo_validade)  values("torta de laranja", "12.00", '2022-12-12');
